1b85e120a2e91272ad9e7d0378182007c9806c93
KuZu <jakubzuchowski13@gmail.com>
2023-07-25 19:07:32 +0200

    Premake!

---
+++ premake5.lua
@@ -0,0 +1,98 @@
+workspace "kEn"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	startproject "Sandbox"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "kEn"
+	location "kEn"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"KEN_PLATFORM_WIN",
+			"KEN_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "KEN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "KEN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "KEN_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"kEn/vendor/spdlog/include",
+		"kEn/src"
+	}
+
+	links {
+		"kEn"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"KEN_PLATFORM_WIN"
+		}
+
+	filter "configurations:Debug"
+		defines "KEN_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "KEN_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "KEN_DIST"
+		optimize "On"
