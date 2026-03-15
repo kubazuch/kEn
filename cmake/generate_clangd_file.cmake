1466fc1786f32afcbfdcfd6897fb4ea6beaf1316
KuZu <jakubzuchowski13@gmail.com>
2026-03-15 01:37:02 +0100

    Update project structure

--- cmake/generate_clangd_file.cmake
+++ cmake/generate_clangd_file.cmake
@@ -1,28 +1,28 @@
-function(generate_clangd_file COMPILER_FLAGS)
+function(generate_clangd_file COMPILER_FLAGS BINARY_DIR)
   if(CMAKE_HOST_UNIX)
     set(CLANGD_COMPILER_STANDARD "-std=c++23")
   elseif(CMAKE_HOST_WIN32)
     set(CLANGD_COMPILER_STANDARD "/clang:-std=c++23")
   endif()
 
-  string(CONCAT CLANGD_FILE_CONTENT 
+  string(CONCAT CLANGD_FILE_CONTENT
     "Documentation:\n"
     "  CommentFormat: Doxygen\n"
     "CompileFlags:\n"
-    "  CompilationDatabase: build/debug/\n"
+    "  CompilationDatabase: ${BINARY_DIR}/\n"
     "  Add:\n"
     "    - "
     ${CLANGD_COMPILER_STANDARD}
     "\n")
-  
+
     foreach(FLAG ${COMPILER_FLAGS})
-      string(CONCAT CLANGD_FILE_CONTENT 
+      string(CONCAT CLANGD_FILE_CONTENT
       "${CLANGD_FILE_CONTENT}"
       "    - "
       ${FLAG}
       "\n")
     endforeach()
-    
+
 
   file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/.clangd "${CLANGD_FILE_CONTENT}")
 endfunction()
